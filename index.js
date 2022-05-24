854a8f352768f9ccf9ce36c587465a3ff31e387d
ArturoMendozaA <arturo_mendoza@outlook.com>
2022-05-23 22:18:48 -0500

    Variables lecture

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 //first line of code
 console.log('Hello world');  
+
+let names = 'Mash';
+console.log(names);
+
+//Cannot be a reserved word
+//should be meaningfu
+//Cannot start with a number (1name)
+//Cannot contain a space or hypen (-)
+// Are case-sensitive
+// declare each variable on a single line
+let firsName = 'Mash';
+let lastName = 'Order'; 
