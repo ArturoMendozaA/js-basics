ab2b3a97c56e87496eeb7895e716769a35e1f052
ArturoMendozaA <arturo_mendoza@outlook.com>
2022-05-24 17:19:26 -0500

    Add types of value lesson

---
+++ typesOfFunctions.js
@@ -0,0 +1,16 @@
+//We can use template literals para no usar espacios y que nuestro código se vea mejor
+//Performing a task
+function greet(name, lastName) {
+    console.log('Hello, ' + name + ' ' + lastName)
+}
+// Calculates a value
+function square(number){
+    return number * number;
+}
+
+let number = square(2);
+console.log(number)
+
+
+
+greet('Jaimito', 'Apeido')
