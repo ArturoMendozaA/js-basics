427a8d86f33a743efb46f19c7b4e9afaee7b88d3
ArturoMendozaA <arturo_mendoza@outlook.com>
2022-05-23 22:37:08 -0500

    more notes

--- primitiveTypes.js
+++ primitiveTypes.js
@@ -5,4 +5,4 @@ let names = 'Mash' //String Literal
 let age = 30 // Number litesl
 let isAproved = true; //Boolean Literal
 let firstName= undefined;// not very common, but it is a value type
-let lastName = null;// use when we want to clear the variable's value
+let lastName = null;// use when we want to clear the variable's value, the type of null is an object
