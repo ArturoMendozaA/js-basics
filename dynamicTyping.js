d0abfe0cab30e50f0b787add399b4d6376fcc628
ArturoMendozaA <arturo_mendoza@outlook.com>
2022-05-23 22:36:07 -0500

    mas apuntes

---
+++ dynamicTyping.js
@@ -0,0 +1,8 @@
+// JS is a dinamic language
+//the type of a variable can change
+let name = 'Mash'
+let age = 30
+// el tipo de valor de la variable se determina durante el runtime
+// todos los números son números
+
+//Reference types
--- primitiveTypes.js
+++ primitiveTypes.js
@@ -4,5 +4,5 @@ let values = '';
 let names = 'Mash' //String Literal
 let age = 30 // Number litesl
 let isAproved = true; //Boolean Literal
-let firstName= undefined;// not very common
+let firstName= undefined;// not very common, but it is a value type
 let lastName = null;// use when we want to clear the variable's value
