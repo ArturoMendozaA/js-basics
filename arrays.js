6a63f992fb7551ad192f727bb226a8d47b5fa74f
ArturoMendozaA <arturo_mendoza@outlook.com>
2022-05-24 14:07:41 -0500

    add array stuff

---
+++ arrays.js
@@ -0,0 +1,8 @@
+//Array- a data structure to strore information
+//tecnicamente un array es un objeto
+
+let selectedColors = ['red', 'blue'];
+selectedColors[2] = 'green'
+selectedColors[3] = '3'
+
+console.log(selectedColors.filter(pan => pan.includes('n')))
