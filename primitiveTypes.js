0e971f3180a877fb796158fb4ab6b9ea99c5e5b2
ArturoMendozaA <arturo_mendoza@outlook.com>
2022-05-23 22:30:49 -0500

    adds primitivetypes topic

---
+++ primitiveTypes.js
@@ -0,0 +1,8 @@
+let values = ''; 
+//Primitives también llamados Value Types
+
+let names = 'Mash' //String Literal
+let age = 30 // Number litesl
+let isAproved = true; //Boolean Literal
+let firstName= undefined;// not very common
+let lastName = null;// use when we want to clear the variable's value
