3b38c745add22546b6e80214942e8b55c688b812
ArturoMendozaA <arturo_mendoza@outlook.com>
2022-05-24 10:29:29 -0500

    Add object lesson

---
+++ objects.js
@@ -0,0 +1,14 @@
+// Reference types los otros tipos eran Primitives value types
+//Reference types:
+// Object 
+//{} Object literal
+// keys are the properties of the object
+// Array
+// Function
+
+let person = {
+    // one or more key properties
+    name: 'Mash',
+    age: 18
+}
+console.log(person)
